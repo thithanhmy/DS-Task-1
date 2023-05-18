9a6b86b8e39a6b3e99ef9293eb1f8e4b51bc0696
thithanhmy <133974936+thithanhmy@users.noreply.github.com>
2023-05-19 00:19:43 +0100

    Add files via upload

---
+++ hello_world.py
@@ -0,0 +1,14 @@
+# Requests user's name input
+name = input('Please enter your name: ')
+
+# Prints name
+print(name)
+
+# Requests the user's age input
+age = input('Please enter your age: ')
+
+# Prints age
+print(age)
+
+# Prints string "Hello World!"
+print('Hello World!')
---
+++ menu.py
@@ -0,0 +1,10 @@
+# Ask user for their order items
+item_1 = input('Please enter your first order: ')
+item_2 = input('Please enter your second order: ')
+item_3 = input('Please enter your third order: ')
+
+# Print confirmation of the user's orders
+print('Order confirmation! You have ordered:')
+print(item_1)
+print(item_2)
+print(item_3)
